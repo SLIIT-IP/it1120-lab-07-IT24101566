3bd0c20385fdea8b96f5b23fe9d6e9ff24298dcf
IT24101566 <IT24101566@my.sliit.lk>
2024-09-24 21:57:54 +0530

    IT24101566Lab7Q2A

---
+++ Q2/IT24101566Lab7Q2A.java
@@ -0,0 +1,14 @@
+public class IT24101566Lab7Q2A
+{
+  public static void main(String args[])
+  {
+      System.out.println("Enter marks for four subjects:");
+ 
+
+      for (int i=1; i<=4; i++)
+      {
+        System.out.print("$ $ $ $"); 
+        System.out.println();
+      }
+    }
+}
